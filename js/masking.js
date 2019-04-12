9d4b6827b9ca572e135204f10dbfaf3acd70069f
代桂林 <daigl@visionet.com.cn>
2019-04-12 22:08:24 +0800

    漏写页面补充

---
+++ js/masking.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+     // $('#sss').mask('00-0000000-0');
+     // $('#tin').mask('000-000-000-000');
+     // //  $('#cellNumber').mask('0000-0000000');
+     // $('#cellNumber').mask("0#");
+     // $('#loanAmount').mask("#,##0.00", {reverse: true});
+     // $('#monthlyIncome').mask("#,##0.00", {reverse: true});
+ });
